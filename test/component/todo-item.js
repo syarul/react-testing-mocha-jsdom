2521ddde61ce6eea302a671dd9ba0c2b1a168632
Shahrul Nizam <hottincup@gmail.com>
2015-09-06 11:25:05 +0800

    reassigned jsdom

--- test/component/todo-item.js
+++ test/component/todo-item.js
@@ -1,29 +1,50 @@
 var React = require('react/addons'),
     assert = require('assert'),
     TodoItem = require('../../common/components/todo-item'),
-    TestUtils = React.addons.TestUtils;
+    TestUtils = React.addons.TestUtils,
+    jsdom = require('jsdom').jsdom;
 
-describe('Todo-item component', function(){
-  before('render and locate element', function() {
-    var renderedComponent = TestUtils.renderIntoDocument(
-      <TodoItem done={false} name="Write Tutorial"/>
-    );
+var Testlib = {
+    renderJSX: function (jsx, context) {
+        return Testlib.renderComponent(React.createClass({
+            displayName: 'TestJSX',
+            render: function () {return jsx;}
+        }), undefined, context);
+    },
+    renderComponent: function (react, props, context) {
+        var rendered;
+        global.document = jsdom('<!DOCTYPE html><html><body></body></html>');
+        global.window = document.parentWindow;
+        rendered = TestUtils.renderIntoDocument(React.createElement(react, props));
+        return TestUtils.findRenderedComponentWithType(rendered, react);
+    }
+};
 
-    // Searching for <input> tag within rendered React component
-    // Throws an exception if not found
-    var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
-      renderedComponent,
-      'input'
-    );
+describe('Todo-item component', function () {
+    before('render and locate element', function () {
+        var node = Testlib.renderJSX(<TodoItem done={false} name="Write Tutorial"/>);
 
-    this.inputElement = inputComponent.getDOMNode();
-  });
+        // Searching for <input> tag within rendered React component
+        // Throws an exception if not found
+        var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
+          node,
+          'input'
+        );
+        this.inputElement = inputComponent.getDOMNode();
+        //console.log(this.inputElement);
 
-  it('<input> should be of type "checkbox"', function() {
-    assert(this.inputElement.getAttribute('type') === 'checkbox');
-  });
+    });
 
-  it('<input> should not be checked', function() {
-    assert(this.inputElement.checked === false);
-  });
+    it.skip('should not show coverage info for Todo-item component', function() {
+        console.log('test2.jsx is not included in coverage report');
+    });
+
+    it('<input> should be of type "checkbox"', function() {
+        assert(this.inputElement.getAttribute('type') === 'checkbox');
+    });
+
+    it('<input> should not be checked', function() {
+        assert(this.inputElement.checked === false);
+    });
+
 });
